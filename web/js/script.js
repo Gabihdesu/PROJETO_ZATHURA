4d52a8c5141e71606c3dfdf3b741f55d6f153d0b
Gabihdesu <gabrielademelosantiago@outlook.com>
2023-05-17 15:09:12 -0300

    first commit

---
+++ web/js/script.js
@@ -0,0 +1,14 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide ({
+        slide: "elenco", //nome do atributo data-slide
+        auto: true, //slide deve passar automaticamente (ja vai automatico ñ precisa colocar )
+        time: 5000 // tempo de transição dos slides (5000 = 5 segundos)
+    });
+
+}
+
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
